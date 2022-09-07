21ace49a18ef9aa45b6384d8b826e05171c04294
Selena <selen15@yandex.ru>
2022-09-07 23:11:10 +0500

    the sum of the sequence numbers ^n$

---
+++ HWTask3.py
@@ -0,0 +1,10 @@
+# Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+
+n = int(input('Введите число: ')) 
+
+def sequence(n):
+
+    return[round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]   
+        
+print(sequence(n))
+print('Сумма последовательности =', round(sum(sequence(n))))
